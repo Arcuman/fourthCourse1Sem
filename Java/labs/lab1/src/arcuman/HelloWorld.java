2d39fcf04cf46c28f15fc92658cc2c62a2a945a4
Arcuman <anton.arcuman.borisov@gmail.com>
2021-09-07 10:16:12 +0300

    first commit

---
+++ Java/labs/lab1/src/arcuman/HelloWorld.java
@@ -0,0 +1,7 @@
+package arcuman;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Services/labs/lab1/WebApplication1/WebApplication1/GetHtmlHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class GetHtmlHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var req = context.Request;
+            HttpResponse res = context.Response;
+
+            res.AppendHeader("Access-Control-Allow-Origin", "*");
+            res.AppendHeader("Access-Control-Allow-Credentials", "true");
+            res.AppendHeader("Access-Control-Allow-Methods", "*");
+            res.WriteFile("HtmlPage1.html");
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
