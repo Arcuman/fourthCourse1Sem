e29beef2ed64fc251f8ab06195cb4ab147968344
Arcuman <anton.arcuman.borisov@gmail.com>
2021-09-20 21:15:51 +0300

    asp 2

---
+++ Java/labs/lab22/src/Sss.java
@@ -0,0 +1,57 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Sss extends HttpServlet {
+    public Sss() {
+        super();
+
+        System.out.println("constructor");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        System.out.println("init");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+
+        System.out.println("destroy");
+    }
+
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        super.service(req, res);
+
+        res.getWriter().write("service");
+        res.getWriter().write(req.getMethod());
+
+        System.out.println("service: getServiceURI()" + req.getRequestURI());
+        System.out.println("service: getServletPath()" + req.getServletPath());
+        System.out.println("service: getRequestURL()" + req.getRequestURL());
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.getWriter().println("Sss" + req.getMethod() + "\n" +
+                "First Name = " + req.getParameter("firstname") + "\n" +
+                "Last Name = " + req.getParameter("lastname") );
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("doGet()");
+
+        res.getWriter().println("Sss" + req.getMethod() + "\n" +
+                "First Name = " + req.getParameter("firstname") + "\n" +
+                "Last Name = " + req.getParameter("lastname") + "\n" +
+                "getQueryString(): " + req.getQueryString());
+    }
+}
---
+++ Services/labs/lab2/lab2/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace lab2.Controllers
+{
+    
+    public class ValuesController : ApiController
+    {
+        public object Get()
+        {
+            int top;
+            if (Result.stack.TryPeek(out top))
+                return new { result = Result.result + top, stack = Result.stack };
+            else
+                return new { result = Result.result, stack = "Stack is empty" };
+        }
+
+        [HttpPost]
+        public object Post([FromUri] string result)
+        {
+            int number;
+            if (int.TryParse(result, out number))
+            {
+                Result.result = number;
+                int top;
+                if (Result.stack.TryPeek(out top))
+                    return new { result = Result.result + top, stack = Result.stack };
+                else
+                    return new { result = Result.result, stack = "Stack is empty" };
+            }
+            else
+                return new { error = new { message = "Type of Params is not Integer", result = result } };
+        }
+
+        [HttpPut]
+        public object Put([FromUri] string add)
+        {
+            int number;
+            if (int.TryParse(add, out number))
+            {
+                Result.stack.Push(number);
+                int top;
+                if (Result.stack.TryPeek(out top))
+                    return new { result = Result.result + top, stack = Result.stack };
+                else
+                    return new { result = Result.result, stack = "Stack is empty" };
+            }
+            else
+                return new { error = new { message = "Type of Params is not Integer", result = add } };
+        }
+
+        [HttpDelete]
+        public object Delete()
+        {
+            int top;
+            if (Result.stack.TryPop(out top) && Result.stack.TryPeek(out top))
+                return new { result = Result.result + top, stack = Result.stack };
+            else
+                return new { result = Result.result, stack = "Stack is empty" };
+        }
+    }
+}
