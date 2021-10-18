66b5beb2797039eaffdf238c57356a1b675634d5
Arcuman <anton.arcuman.borisov@gmail.com>
2021-10-18 09:17:31 +0300

    sd

---
+++ Java/labs/lab11/src/by/belstu/servlet/SssHeader.java
@@ -0,0 +1,22 @@
+package by.belstu.servlet;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SssHeader extends HttpServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            Integer x = Integer.parseInt(request.getHeader("Value-X"));
+            Integer y = Integer.parseInt(request.getHeader("Value-Y"));
+
+            Thread.sleep(7000);
+
+            response.setHeader("Value-Z", Integer.toString(x + y));
+        } catch (Exception e) {
+            response.getWriter().println(e.getMessage());
+        }
+    }
+}
